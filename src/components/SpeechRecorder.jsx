049b510586b2a64672453c6978ff440e177290d1
Payal Sonawane <sonawanepayal2005@gmail.com>
2026-02-19 16:06:15 +0530

    case issue fixed

--- src/components/SpeechRecorder.jsx
+++ src/components/SpeechRecorder.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { sendAudio, getTranscripts, deleteTranscript } from "./services/api";
+import { sendAudio, getTranscripts, deleteTranscript } from "../services/Api";
 
 const SpeechRecorder = () => {
   const [text, setText] = useState("");
